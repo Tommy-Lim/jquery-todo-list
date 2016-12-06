2c5c9908f67b8e03f1990b8f043118486290d937
Tommy-Lim <tlim24@gmail.com>
2016-12-05 23:37:58 -0800

    html, css, js and jquery complete

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$("#container-input").on("click", ".inputButton", function(){
+  addItem();
+});
+
+$("#inputBox").on("keypress", function(e){
+  if(e.which==13||e.keyCode==13){
+    addItem();
+  }
+});
+
+$("#container-entries").on("click", ".deleteButton", function(){
+  removeItem($(this));
+});
+
+
+var addItem = function(){
+  if($('#inputBox').val()===""){
+    //do nothing
+  } else{
+    var divToAdd = $("<div class='entry'><div class='entryText'></div><button class='deleteButton'>Done</button></div>");
+    var textToAdd = $("#inputBox").val();
+    $("#container-entries").prepend(divToAdd);
+    $("#container-entries .entryText").first().text(textToAdd);
+    $("#inputBox").val("");
+    $("#inputBox").focus();
+  }
+};
+
+var removeItem = function(event){
+  event.parent().remove();
+};
